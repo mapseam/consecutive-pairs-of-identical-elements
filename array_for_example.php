4951581d3704fc99c40fe58a15fa069289b38d59
Mapseam <37932909+mapseam@users.noreply.github.com>
2020-03-27 20:56:55 +0400

    Initial version of the file

---
+++ array_for_example.php
@@ -0,0 +1,15 @@
+<?php
+$arr = array(1, 1, 2, 3, 4, -51, 12, 12, 12, -51);
+$counter = 0;
+$prev = arr[0];
+
+$length = count($arr);
+for($i = 1; $i < $length; $i++) {
+	if $arr[$i] == $prev
+		$counter++;
+	else
+		$prev = $arr[$i]
+}
+
+echo $counter;
+?>
